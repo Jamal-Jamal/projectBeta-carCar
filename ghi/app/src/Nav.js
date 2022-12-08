d51264f64efc6dd917f7678c646ffd793aca6109
Steven Duong <stevenduong5@gmail.com>
2022-12-07 18:11:50 -0800

    steven before merge of microservices

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -12,7 +12,7 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item dropdown ">
+            <li className="nav-item dropdown ">
               <a
                 className="nav-link dropdown-toggle"
                 role="button"
@@ -20,35 +20,35 @@ function Nav() {
                 aria-expanded="false">
                 Sales
               </a>
-              <ul className="dropdown-menu">
-                <li className="nav-item">
-                  <NavLink className="dropdown-item" to="/salespeople">
-                  Add Sales Person
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="dropdown-item" to="/customers">
-                  Add customer
-                  </NavLink>
-                </li>
-                <div className="dropdown-divider"></div>
-                <li className="nav-item">
-                  <NavLink className="dropdown-item" to="/salesrecords/new">
-                  Create a Sale Record
-                  </NavLink>
-                </li>
-                <div className="dropdown-divider"></div>
-                <li className="nav-item">
-                  <NavLink className="dropdown-item" to="/salesrecords">
-                  Sales Record
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="dropdown-item" to="/salehistory">
-                  Sales History
-                  </NavLink>
-                </li>
-              </ul>
+          <ul className="dropdown-menu">
+            <li className="nav-item">
+                <NavLink className="dropdown-item" to="/salespeople">
+                Add Sales Person
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="dropdown-item" to="/customers">
+                Add customer
+                </NavLink>
+            </li>
+            <div className="dropdown-divider"></div>
+            <li className="nav-item">
+                <NavLink className="dropdown-item" to="/salesrecords/new">
+                Create a Sale Record
+                </NavLink>
+            </li>
+            <div className="dropdown-divider"></div>
+            <li className="nav-item">
+                <NavLink className="dropdown-item" to="/salesrecords">
+                Sales Record
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="dropdown-item" to="/salehistory">
+                Sales History
+                </NavLink>
+            </li>
+            </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Inventories</a>
